fe2c13a820bae7c00b64706feb991d4f207d7457
Anthony Kim <fbssaejun@gmail.com>
2021-10-19 12:41:14 -0400

    add anchor tags to navigate user to activity page

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,15 @@ import ReactDOM from 'react-dom';
 import Header from './Header.jsx';
 import Footer from './Footer.jsx';
 import Home from './components/Home.jsx'
-import { Route, Switch, BrowserRouter, useLocation } from 'react-router-dom';
+import { Route, Switch, BrowserRouter, useLocation, Link } from 'react-router-dom';
 
 
 const App = () => {
+  
+  const location = useLocation();
+  console.log(location)
+
+  
   return (
     <div className='container'>
       <Header/>
@@ -15,6 +20,8 @@ const App = () => {
         <Switch location={location} key={location.pathname}>
           <Route path="/" exact>
             <Home/>
+          </Route>
+          <Route path="/activity">
           </Route>
         </Switch>
       </div>
--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import './css/footer.css';
+import { Link } from 'react-router-dom';
 
 export default function Footer () {
   return (
     <footer>
-      <button><i className="fas fa-phone"></i></button>
-      <button><i className="far fa-user"></i></button>
-      <button className="number-pad"><i className="fas fa-grip-vertical"></i></button>
-      <button><i className="fas fa-cog"></i></button>
-      <button><i className="far fa-circle"></i></button>
+      <Link to="/activity"><i className="fas fa-phone"></i></Link>
+      <Link><i className="far fa-user"></i></Link>
+      <Link className="number-pad"><i className="fas fa-grip-vertical"></i></Link>
+      <Link><i className="fas fa-cog"></i></Link>
+      <Link><i className="far fa-circle"></i></Link>
     </footer>
   );
 }
