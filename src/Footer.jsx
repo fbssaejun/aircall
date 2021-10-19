3a412c4b0007ee0a99a097c09a52badcb8d9b019
Anthony Kim <fbssaejun@gmail.com>
2021-10-18 21:53:18 -0400

    add footer

---
+++ src/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './css/footer.css';
+
+export default function Footer () {
+  return (
+    <footer>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+    </footer>
+  );
+}
