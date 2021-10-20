5f05180d8bcbb9cfc2d5474a20794952ed60d049
Anthony Kim <fbssaejun@gmail.com>
2021-10-19 20:27:13 -0400

    add call details

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -18,7 +18,7 @@ export default function Activity () {
   }, [])
 
   const callFeed = calls.map(call => {
-    return <Calls type={call.call_type} createdAt={call.created_at} duration={call.duration} from={call.from} direction={call.direction} isArchived={call.is_archived} via={call.via}/>
+    return <Calls type={call.call_type} createdAt={call.created_at} duration={call.duration} from={call.from} direction={call.direction} isArchived={call.is_archived} via={call.via} to={call.to}/>
   })
 
   return (
--- src/components/Calls.jsx
+++ src/components/Calls.jsx
@@ -1,29 +1,27 @@
 import React from "react";
 import "../css/calls.css";
 import { useState } from "react";
-import Box from '@mui/material/Box';
-import Modal from '@mui/material/Modal';
-import Typography from '@mui/material/Typography';
-
-
-
+import Box from "@mui/material/Box";
+import Modal from "@mui/material/Modal";
+import Typography from "@mui/material/Typography";
 
 export default function Calls(props) {
-  const { direction, type, createdAt, duration, from, isArchived, via } = props;
+  const { direction, type, createdAt, duration, from, isArchived, via, to } =
+    props;
 
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-
   const style = {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%, -50%)',
-    width: 400,
-    bgcolor: 'background.paper',
-    border: '2px solid #000',
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 300,
+    bgcolor: "background.paper",
+    border: "1px solid #000",
+    borderRadius: "10px",
     boxShadow: 24,
     p: 4,
   };
@@ -31,38 +29,39 @@ export default function Calls(props) {
   return (
     <div>
       {createdAt}
-    <button onClick={()=>handleOpen()}>
-      <div className="individual-call">
-        <div>
-          <span className="material-icons">local_phone</span>
-          {direction === "inbound" ? (
-            <span className="material-icons received">call_received</span>
-          ) : (
-            <span className="material-icons made">call_made</span>
-          )}
+      <button className="individual-call-btn" onClick={() => handleOpen()}>
+        <div className="individual-call">
+          <div>
+            <span className="material-icons">local_phone</span>
+            {direction === "inbound" ? (
+              <span className="material-icons received">call_received</span>
+            ) : (
+              <span className="material-icons made">call_made</span>
+            )}
+          </div>
+          <div className="call-info">
+            <span className="call-from-name">{from}</span>
+            <p>Tried to call via {via}</p>
+          </div>
+          <div>
+            <p>{createdAt}</p>
+          </div>
         </div>
-        <div className="call-info">
-          <span className="call-from-name">{from}</span>
-          <p>Tried to call via {via}</p>
-        </div>
-        <div>
-          <p>{createdAt}</p>
-        </div>
-      </div>
-    </button>
-    <Modal
+      </button>
+      <Modal
         open={open}
         onClose={handleClose}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
-        <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+        <Box sx={style} className="call-details">
+          <h2>Call details</h2>
+          <span>From: {from}</span>
+          <span>To: {to}</span>
+          <span>Call type: {type}</span>
+          <span>Duration: {duration} min</span>
+          <span>Archived: {isArchived.toString()}</span>
+          <span>Via: {via}</span>
         </Box>
       </Modal>
     </div>
